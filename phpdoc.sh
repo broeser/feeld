d66886d2f2d8cc98fdb5852efb91a7647d6ec5c7
Benedict Roeser <mail@benedictroeser.de>
2016-01-24 18:12:43 +0100

    Tried – without luck – to fix a problem in the generated docs

--- phpdoc.sh
+++ phpdoc.sh
@@ -3,5 +3,5 @@
 _script=$0
 DIR="$(dirname $_script)"
 
-/home/benedict/.composer/vendor/bin/phpdoc run -vvv --ansi --progressbar --directory $DIR/src --target $DIR/doc --title feeld
+phpdoc run --ansi --progressbar --directory $DIR/src --target $DIR/doc --title feeld
 echo $DIR
